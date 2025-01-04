c66608b3f75ba544c0a5e111cc69a1f5fba8577a
Ken Muyesu <c1t1z3nk3n@gmail.com>
2025-01-04 07:40:10 +0300

    chore: adding persistence with SQLite

--- cmd/web-app/main.go
+++ cmd/web-app/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/citixenken/url-shortener-app/internal/controllers"
+	"github.com/citixenken/url-shortener-app/internal/db"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", controllers.ShowIndex)
-	http.HandleFunc("/shorten", controllers.Shorten)
+	slite, err := sql.Open("sqlite3", "db.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer slite.Close()
+
+	if err := db.CreateTable(slite); err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Path == "/" {
+			controllers.ShowIndex(writer, request)
+		} else {
+			controllers.Proxy(slite)(writer, request)
+		}
+	})
+	http.HandleFunc("/shorten", controllers.Shorten(slite))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
